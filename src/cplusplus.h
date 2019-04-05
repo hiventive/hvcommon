7523235c26bab3e8f89a8ef2928b4d91c6c42b20
Guillaume Delbergue <guillaume.delbergue@hiventive.com>
2019-04-05 13:42:57 +0200

    add cplusplus macros
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@hiventive.com>

---
+++ src/cplusplus.h
@@ -0,0 +1,18 @@
+/*
+ * @file cplusplus.h
+ * @author Guillaume Delbergue <guillaume.delbergue@hiventive.com>
+ * @date March, 2019
+ * @copyright Copyright (C) 2019, Hiventive.
+ *
+ * @brief Common C++ macros
+ */
+
+#if defined(_MSC_VER) && _MSC_VER <= 1700
+  #define HV_CPLUSPLUS_MEMBER_FUNCTION_DEFAULT {}
+  #define HV_CPLUSPLUS_MEMBER_FUNCTION_DELETE
+#else
+  #define HV_CPLUSPLUS_MEMBER_FUNCTION_DEFAULT = default
+  #define HV_CPLUSPLUS_MEMBER_FUNCTION_DELETE = delete
+#endif
+
+
